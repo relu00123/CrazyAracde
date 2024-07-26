dc36c0693e905320d5f1e6c93167f96f538f7c7d
GeonHee <relu00123@gmail.com>
2024-07-26 20:54:04 +0900

    Lobby Room UI (Grid Panel 작업중)

---
+++ Client/Assets/Scripts/UI/Scene/Lobby/UI_RoomItem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UI_RoomItem : UI_Base
+{
+    public override void Init()
+    {
+        
+    }
+}
